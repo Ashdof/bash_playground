79fe56d7e42d8313e1e3d98a61d2cad31d8971c4
Ashdof <mzigah4@gmail.com>
2024-02-23 14:35:08 +0000

    More lessons

---
+++ 6_functions/0_function.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# demonstrate function
+
+print_something() {
+	echo Hello, I\'m a function!
+}
+
+print_something
+print_something
---
+++ 6_functions/1_function.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+print_something() {
+	echo Hello, $1!
+}
+
+print_something Jay
+print_something Kay
